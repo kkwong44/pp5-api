ae7bd57fdabb8b840d40091ce436a0bb84262a82
kkwong44 <wong.k@sky.com>
2022-09-03 02:38:32 +0000

    update api serializer

--- pp5_api/serializers.py
+++ pp5_api/serializers.py
@@ -3,12 +3,11 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    is_staff = serializers.ReadOnlyField(source='profile.owner.is_staff')
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'is_staff', 'profile_id', 'profile_image'
+            'profile_id', 'profile_image'
         )
     
