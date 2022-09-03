4f7028d62cb26223c3decdf1ff474699b96c824b
kkwong44 <wong.k@sky.com>
2022-09-03 00:21:40 +0000

    Update post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import Post
 from likes.models import Like
 
+from django.contrib.auth.models import User
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    staff = serializers.ReadOnlyField(source='owner.is_staff')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
@@ -43,7 +45,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at',
+            'id', 'owner', 'staff', 'is_owner', 'profile_id', 'profile_image', 'created_at',
             'updated_at', 'title', 'content', 'image', 'image_filter', 'like_id',
             'likes_count', 'comments_count',
         ]
